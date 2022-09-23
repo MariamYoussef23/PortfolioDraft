e08f6082f385f8f47426af381a7f94f17ac4488d
MariamYoussef23 <mariamyoussef@aucegypt.edu>
2022-09-23 20:12:36 +0200

    skills icons

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-
-import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import { Container } from '@/components/Container'
 
@@ -10,7 +8,18 @@ const icons = [
   {name: "HTML5", src:"/icons/html.svg", alt:"html Logo" }, 
   {name: "CSS3", src:"/icons/css.svg", alt:"CSS Logo" }, 
   {name: "javascript", src:"/icons/javascript.svg", alt:"javascript Logo" }, 
-  {name: "bootstrap", src:"/icons/bootstrap.svg", alt:"CSbootstrapS Logo" }, 
+  {name: "bootstrap", src:"/icons/bootstrap.svg", alt:"bootstrap Logo" }, 
+  {name: "next.js", src:"/icons/next-js.svg", alt:"next.js Logo" }, 
+  {name: "formik", src:"/icons/formik.svg", alt:"formik Logo" }, 
+  {name: "node", src:"/icons/node.svg", alt:"node Logo" }, 
+  {name: "postgresql", src:"/icons/postgresql.svg", alt:"postgresql Logo" }, 
+  {name: "prisma", src:"/icons/prisma.svg", alt:"prisma Logo" }, 
+  {name: "react", src:"/icons/react.svg", alt:"react Logo" }, 
+  {name: "redux", src:"/icons/redux.svg", alt:"redux Logo" }, 
+  {name: "socket-io", src:"/icons/socket-io.svg", alt:"socket-io Logo" }, 
+  {name: "supabase", src:"/icons/supabase.svg", alt:"supabase Logo" }, 
+  {name: "tailwind", src:"/icons/tailwind.svg", alt:"tailwind Logo" }, 
+  {name: "typescript", src:"/icons/typescript.svg", alt:"typescript Logo" }, 
 ]
 
 export default function Skills() {
@@ -36,9 +45,9 @@ export default function Skills() {
       {icons.map((icon) => (
         <div key={Math.random()}>
           <div className=" flex items-center space-x-3 rounded-lg px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 ">
-          <div className="box-icon">
+          <div className="text-center">
             <img src={icon.src} alt={icon.alt}/>
-            <p>{icon.name}</p>
+            <p className="mt-6 space-y-7 text-base font-bold text-zinc-600 dark:text-zinc-400">{icon.name}</p>
           </div>
           </div>
           
